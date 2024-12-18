892e34929603d3e768794cd3a38cfc1f255e489a
MichaelPKamm <michael.kamm.5@gmail.com>
2024-12-18 15:02:04 +0100

    2Change
    Test

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import "leaflet/dist/leaflet.css";
 import dynamic from "next/dynamic";
 
-const Map = dynamic(() => import("./maps/QuakesMap"), { ssr: false });
+const Map = dynamic(() => import("./QuakesMap"), { ssr: false });
 
 export default function App() {
   return (
